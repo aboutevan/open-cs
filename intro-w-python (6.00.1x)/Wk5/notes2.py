66f5173a10bf58e3d86a02d1b8c6a9254525bb13
Evan Salzbrenner <salzbrenner@urbaninsight.com>
2018-06-15 09:12:01 -0700

    wk5 done

--- intro-w-python (6.00.1x)/Wk5/notes2.py
+++ intro-w-python (6.00.1x)/Wk5/notes2.py
@@ -2,3 +2,28 @@
 
 
 """
+def genPrimes():
+    x = 2
+    primes = [2]
+
+    yield x
+    while True:
+        x += 1
+        if all((x % p) != 0 for p in primes):
+            primes.append(x)
+            yield x
+
+x = genPrimes()
+
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
+x.__next__()
